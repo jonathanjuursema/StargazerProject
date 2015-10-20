e8a249694306aa374340f2e95237827649c83575
Jonathan Juursema <github@jonathanj.nl>
2015-10-20 14:14:24 +0200

    Test SPI interface embedded.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var socketio = require('socket.io')(http);
+var spi = require('spi');
 
 /* Loading local modules. */
 var coreserver = require('./coreserver.js');
@@ -28,3 +29,17 @@ http.listen(process.env.PORT || 5000);
 socketio.on('connection', function(socket) {
   console.info("A new connection was established.");
 });
+
+/* Establishing SPI connection */
+var soc = new spi.Spi('/dev/spidev0.0', {}, function(s){s.open();});
+
+setInterval(function() {
+  soc.transfer(new Buffer([ 0x00 ]), new Buffer([ 0x00 ]), function(dev, buf) {
+    console.info("SPI: Test 00 sent.");
+    setTimeout(function() {
+      soc.transfer(new Buffer([ 0x42 ]), new Buffer([ 0x00 ]), function(dev, buf) {
+        console.info("SPI: Test 42 sent.");
+      });
+    }, 1000);
+  });
+}, 2000)
